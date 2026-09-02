cf2ac91a8146962c102869f819af7591f7e0dd27
craigcossairt <craigcossairt@gmail.com>
2026-09-02 02:15:47 -0600

    Stop shipping a stale Vercel output so production actually builds and can call Resend.

--- .vercel/output/functions/__server.func/_ssr/frames.comments-CncGhwGg.mjs
+++
@@ -1,9 +0,0 @@
-import { s as require_jsx_runtime } from "../_libs/@radix-ui/react-collection+[...].mjs";
-import { t as CommentsFrame } from "./thread-B38kd3DY.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/frames.comments-CncGhwGg.js
-var import_jsx_runtime = require_jsx_runtime();
-function CommentsRoute() {
-	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(CommentsFrame, {});
-}
-//#endregion
-export { CommentsRoute as component };
--- .vercel/output/static/assets/frames.comments-ru0eJUT9.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-CznXPbDH.js";import{t}from"./thread-5jkbuFIB.js";var n=e();function r(){return(0,n.jsx)(t,{})}export{r as component};
